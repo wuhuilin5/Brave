fbb5f44ef448da9bbd9e318b05ae0cb4720360ad
wuhuilin5 <huilin.wu@hotmail.com>
2014-10-08 11:34:03 +0800

    ad

---
+++ Classes/MainScene.h
@@ -0,0 +1,40 @@
+#ifndef __MAIN_SCENE_H__
+#define __MAIN_SCENE_H__
+
+#include "cocos2d.h"
+
+USING_NS_CC;
+
+class Player;
+
+class MainScene : public cocos2d::Layer
+{
+public:
+    // there's no 'id' in cpp, so we recommend returning the class instance pointer
+    static cocos2d::Scene* createScene();
+
+    // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
+    virtual bool init();  
+    
+    // a selector callback
+    void menuCloseCallback(cocos2d::Ref* pSender);
+    
+    // implement the "static create()" method manually
+    CREATE_FUNC(MainScene);
+
+	virtual void draw(Renderer *renderer, const Mat4 &transform, uint32_t flags) override;
+	
+	virtual bool onTouchBegan(Touch* touch, Event* event) override;
+
+protected:
+	void onDraw(const cocos2d::Mat4 &transform, bool transformUpdate);
+	CustomCommand _customCommand;
+
+private:
+	EventListenerTouchOneByOne* _listener_touch;
+
+	Player* _player;
+	Player* _enemy1;
+};
+
+#endif // __MainScene_SCENE_H__
